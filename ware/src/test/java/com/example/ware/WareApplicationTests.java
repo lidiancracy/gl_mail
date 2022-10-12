a4bee36fa850b135d3f456f0e15a209e147f09fc
lidian <837997288@qq.com>
2022-10-12 20:47:35 +0800

    1_模块构建

---
+++ ware/src/test/java/com/example/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
